d26313ca94a15d2b8d268f9c1ef12acaba8a3c7b
Nelviesh <146974073+Nelviesh@users.noreply.github.com>
2024-11-18 20:40:08 +0530

    Time: 10 ms (79.40%) | Memory: 45.5 MB (40.71%) - LeetSync

--- 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.java
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.java
@@ -1,27 +1,33 @@
+import java.util.*;
+
 class Solution {
     public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        HashMap<Integer,Integer> map2=new HashMap<>();
-        for(int i=0;i<nums1.length;i++){
-            map.put(nums1[i],map.getOrDefault(nums1[i],0)+1);
-        }
-        for(int i=0;i<nums2.length;i++){
-            map2.put(nums2[i],map2.getOrDefault(nums2[i],0)+1);
-        }
-        List<List<Integer>> list=new ArrayList<>();
-        list.add(new ArrayList<>());
-        list.add(new ArrayList<>());
+        Set<Integer> s1 = new HashSet<>();
+        Set<Integer> s2 = new HashSet<>();
+        List<List<Integer>> ans = new ArrayList<>();
+        ans.add(new ArrayList<>());
+        ans.add(new ArrayList<>());
 
-        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
-            if(!map2.containsKey(entry.getKey())){
-                list.get(0).add(entry.getKey());
+        for (int i : nums1) {
+            s1.add(i);
+        }
+        
+        for (int i : nums2) {
+            s2.add(i);
+        }
+
+        for (int i : s1) {
+            if (!s2.contains(i)) {
+                ans.get(0).add(i);
             }
         }
-        for(Map.Entry<Integer, Integer> entry : map2.entrySet()){
-            if(!map.containsKey(entry.getKey())){
-                list.get(1).add(entry.getKey());
+
+        for (int i : s2) {
+            if (!s1.contains(i)) {
+                ans.get(1).add(i);
             }
         }
-        return list;
+
+        return ans;
     }
 }
