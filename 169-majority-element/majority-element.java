71f92335ca571ad68d7417c0f06d643e6c6ec08b
Nelviesh <146974073+Nelviesh@users.noreply.github.com>
2024-11-28 19:31:36 +0530

    Time: 4 ms (53.42%) | Memory: 53.9 MB (5.24%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,21 +1,7 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap<Integer, Integer> HM = new HashMap<>();
-        
-        // Count occurrences of each number
-        for (int x : nums) {
-            HM.put(x, HM.getOrDefault(x, 0) + 1);
-        }
-        
-        // Find the majority element
-        int majorityCount = nums.length / 2;
-        for (int key : HM.keySet()) {
-            if (HM.get(key) > majorityCount) {
-                return key;
-            }
-        }
-        
-        // This line is unreachable if input guarantees a majority element
-        return -1; 
+        Arrays.sort(nums);
+        int n = nums.length;
+        return nums[n/2];
     }
 }
