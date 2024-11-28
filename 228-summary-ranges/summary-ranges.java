96878203dc9eea9866d794b3372122f5e93c7887
Nelviesh <146974073+Nelviesh@users.noreply.github.com>
2024-11-28 21:27:39 +0530

    Time: 5 ms (75.55%) | Memory: 41.9 MB (23.75%) - LeetSync

---
+++ 228-summary-ranges/summary-ranges.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        int size = nums.length;
+        List<String> list = new ArrayList<>();
+        StringBuilder str = new StringBuilder();
+        int i = 0;
+
+        while (i < size) {
+            // Start a new range
+            int start = nums[i];
+            int end = start;
+
+            // Find the end of the current range
+            while (i + 1 < size && nums[i + 1] == nums[i] + 1) {
+                i++;
+                end = nums[i];
+            }
+
+            // Add the range to the result
+            if (start == end) {
+                list.add(String.valueOf(start)); // Single element
+            } else {
+                list.add(start + "->" + end); // Range
+            }
+
+            // Move to the next element
+            i++;
+        }
+        return list;
+    }
+}
