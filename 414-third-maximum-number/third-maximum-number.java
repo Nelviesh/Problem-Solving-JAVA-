ba0267acacc0c0f09f167aaef8f3b9b03e20f231
Nelviesh <146974073+Nelviesh@users.noreply.github.com>
2024-12-03 20:32:04 +0530

    Time: 3 ms (83.83%) | Memory: 43.4 MB (32.57%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.java
+++ 414-third-maximum-number/third-maximum-number.java
@@ -1,23 +1,30 @@
-import java.util.*;
-import java.util.stream.Collectors;
-
 class Solution {
     public int thirdMax(int[] nums) {
-        // Convert the array to a Set to remove duplicates
-        Set<Integer> numbers = Arrays.stream(nums).boxed().collect(Collectors.toSet());
+        // Use three variables to keep track of the top 3 maximums
+        Integer max1 = null, max2 = null, max3 = null;
 
-        // If there are fewer than 3 unique numbers, return the maximum
-        if (numbers.size() < 3) {
-            return Collections.max(numbers);
+        for (int num : nums) {
+            // Skip if this number is already considered
+            if ((max1 != null && num == max1) || 
+                (max2 != null && num == max2) || 
+                (max3 != null && num == max3)) {
+                continue;
+            }
+
+            // Update the top 3 maximums
+            if (max1 == null || num > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = num;
+            } else if (max2 == null || num > max2) {
+                max3 = max2;
+                max2 = num;
+            } else if (max3 == null || num > max3) {
+                max3 = num;
+            }
         }
 
-        // Find the third maximum by repeatedly removing the largest element
-        for (int i = 0; i < 2; i++) {
-            int max = Collections.max(numbers);
-            numbers.remove(max);
-        }
-
-        // The third maximum is now the largest number in the remaining set
-        return Collections.max(numbers);
+        // If we have at least 3 unique maximums, return the third
+        return max3 != null ? max3 : max1;
     }
 }
